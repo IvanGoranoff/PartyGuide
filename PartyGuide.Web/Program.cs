70c0cd38d4f820ef6f2cb52170b7ad0bd272517d
Velimir Ivanov <vivanov@tu-sofia.bg>
2023-12-12 18:38:32 +0200

    Filter system visual implementation

--- PartyGuide.Web/Program.cs
+++ PartyGuide.Web/Program.cs
@@ -66,7 +66,7 @@ internal class Program
 		}
 		else
 		{
-			app.UseExceptionHandler("/Home/Error");
+			app.UseExceptionHandler("/Service/Error");
 			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 			app.UseHsts();
 		}
